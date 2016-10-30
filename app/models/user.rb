e9e4dab0a25185b17fa82f9acf860abbca6e214d
Lauren <laleung94@gmail.com>
2016-10-30 15:13:19 -0400

    Added basic user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,25 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
+  include BCrypt
+
+  validates :name, :email, :password_hash, presence: true
+  validates :email, uniqueness: true
+  has_many :folders, dependent: :destroy
+  has_many :notes, dependent: :destroy
+  validate :valid_email?
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def valid_email?
+    unless name.nil? || name.empty?
+      matches = email.match(/\A[a-z|\d]+[@][a-z]+.[a-z]+\z/)
+      errors.add(:email, 'Invalid email address') if matches.to_s.empty?
+    end
+  end
 end
