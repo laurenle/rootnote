55db3310a94e78bd9fdb324a6915b804bb8865c0
Lauren <laleung94@gmail.com>
2016-10-30 15:31:00 -0400

    Added nav bar, splash page, sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(name: params[:name])
+    if @user.nil? || @user.password != params[:password]
+      render :new
+    else
+      session[:user_id] = @user.id
+      redirect_to @user
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_url
+  end
+end
