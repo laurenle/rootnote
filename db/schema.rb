e32dd918830e81a8b14b29158660c1c21e1a8a0c
Lauren <laleung94@gmail.com>
2017-01-16 16:36:18 -0500

    Switched to postgres, migrations up and running

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161030184337) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "folders", force: :cascade do |t|
     t.string   "name"
     t.integer  "user_id"
