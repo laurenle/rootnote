250f4e1f6ccb4295e8a15232086c7b4a98f0e116
Memoria Matters <memzenator@gmail.com>
2017-02-11 23:27:39 -0500

    Better file naming Saved pdf.name is now original file name, not tempfile name Images during processing are saved with tempfile name to avoid collisions

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -13,24 +13,23 @@ class PdfsController < ApplicationController
   def create
     @pdf = Pdf.new(pdf_params)
     pdf_file = params[:pdf][:file]
-    @pdf.name = File.basename(pdf_file.path)
+    @pdf.name = pdf_file.original_filename
     @pdf.save
 
     # TODO: Lots of error handling
 
     # Convert pdf to images
     pdf_path = File.absolute_path(pdf_file.path)
-    dir = File.dirname(pdf_path)
-    image_path = File.join(dir, "%d.jpg")
-    Paperclip.run("convert", "-density 150 :pdf_path -quality 80 -geometry 500x700 -alpha remove :image_path",
+    image_base_path = File.join(File.dirname(pdf_path), File.basename(pdf_path, ".*"))
+    image_path = "#{image_base_path}%d.jpg"
+    Paperclip.run("convert", "-density 150 :pdf_path -quality 90 -geometry 500x700 -alpha remove :image_path",
                   pdf_path: pdf_path, image_path: image_path)
     
     # Save each page with image attachments
     i = 0;
-    while File.exist?(image_path = File.join(dir, "#{i}.jpg")) do
+    while File.exist?(image_path = "#{image_base_path}#{i}.jpg") do
       image_file = File.open(image_path)
-      doc_page = DocumentPage.new(pdf_id: @pdf.id, number: i, image: image_file)
-      doc_page.save
+      doc_page = DocumentPage.create(pdf_id: @pdf.id, number: i, image: image_file)
       image_file.close
       File.delete(image_path)
       i += 1
